a2908834ae55ba07f00305b21a594da623f0722a
Disco Adina <dadina@si-m1.kzn.21-school.ru>
2021-08-04 22:47:41 +0300

    second try

--- 1.js
+++ 1.js
@@ -6,7 +6,7 @@ let window_width = window.innerWidth
 
 canvas.width = window_width
 canvas.height = window_height
-canvas.style.background = "#ff9"
+canvas.style.background = "white"
 
 class Circle {
 	constructor(xpos, ypos, radius, color) {
@@ -20,9 +20,14 @@ class Circle {
 		ctx.beginPath()
 		ctx.arc(this.xpos, this.ypos, this.radius, 0, Math.PI * 2, false)
 		ctx.stroke()
+		ctx.fillStyle = this.color
+		ctx.fill()
 		ctx.closePath()
+
 	}
 }
+
+let numofcircles = prompt('Сколько кругов?', ' ');
 
 let all_circles = []
 
@@ -30,29 +35,27 @@ let createCircle = function(circle) {
 	circle.draw(ctx)
 }
 
-for ( var numbers = 0; numbers < 5; numbers++ ){
-	let random_x = Math.random() * window.innerWidth
-	let random_y = Math.random() * window.innerHeight
 
-	let my_circle = new Circle(random_x, random_y, 50, "black")
+
+function getRandomRgb() {
+	var num = Math.round(0x00ffffff * Math.random());
+	console.log(num);
+	var r = num >> 16;
+	console.log(r);
+	var g = num >> 8 & 255;
+	console.log(g);
+	var b = num & 255;
+	console.log(b);
+	return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+	
+  }
+  
+
+for ( var numbers = 0; numbers < numofcircles; numbers++ ){
+	let random_x = Math.random() * 1000
+	let random_y = Math.random() * 1000
+
+	let my_circle = new Circle(random_x, random_y, 50, getRandomRgb())
 	all_circles.push(my_circle)
 	createCircle(all_circles[numbers])
 }
-let min = 000
-let max = 256
-
-function getRandomInt(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
-  }
-
-let rgb = Math.floor(Math.random() * (max - min)) + min
-
-console.log(Math.floor(Math.random() * (max - min)) + min)
-
-// let my_circle = new Circle(100, 100, 50, "black")
-// let my_circle2 = new Circle(150, 150, 50, "black")
-
-// my_circle.draw(ctx)
-// my_circle2.draw(ctx)
